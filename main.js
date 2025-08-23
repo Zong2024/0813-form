7949bd9e225288ae49ebe73487dac3ca5323f909
Zong2024 <Zong20021107@gmail.com>
2025-08-23 11:51:55 +0800

    add 新增aos動畫效果

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log('Hello world');
+console.log("Hello world");
+
+AOS.init({
+  offset: 120,
+  duration: 400,
+  once: false,
+  delay: 0,
+});
